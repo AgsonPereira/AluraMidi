2446ffb1a74451d44b9e6bfae9e58a33234f660a
Agson <agson.pereira.14@gmail.com>
2022-03-08 20:33:23 -0300

    Deixando todas a teclas funcionais com seus respectivos sons

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play()
+function tocaSons (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for(let i = 0; i < listaDeTeclas.length; i++){
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;    //template string
+
+    tecla.onclick = function (){
+        tocaSons(idAudio)
+    }
+}
