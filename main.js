ca3593613485b417a9b4948891afbdc717e585d3
Agson <agson.pereira.14@gmail.com>
2022-03-08 22:19:07 -0300

    Adicionando a funcionalidade das teclas enter e espaço para tocar os sons

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSons (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSons (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+      
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 
@@ -7,11 +15,23 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
 for(let i = 0; i < listaDeTeclas.length; i++){
+   
     const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;    //template string
 
     tecla.onclick = function (){
-        tocaSons(idAudio)
+        tocaSons(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
     }
 }
